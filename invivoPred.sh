66c88e7cf8eefae42b25e9488ecda1c91daed5ab
zcemexx <zcemexx@ucl.ac.uk>
2026-03-10 00:23:49 +0000

    Auto-sync 2026-03-10 00:23

---
+++ invivoPred.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#$ -S /bin/bash
+#$ -N invPredR
+#$ -l h_rt=12:59:00
+#$ -l mem=8G
+#$ -l tmpfs=8G
+#$ -pe smp 8
+#$ -wd /home/zcemexx/Scratch
+#$ -o /home/zcemexx/Scratch/logs/
+#$ -j y
+#$ -m abe
+#$ -M zcemexx@ucl.ac.uk
+
+set -euo pipefail
+module purge
+module load matlab/full/r2023a/9.14
+
+DATA_ROOT=/home/zcemexx/Scratch/data/invivo/3T_in-vivo_3DGRE_Head_Neck_repeatibility \
+CODE_DIR=/home/zcemexx/projects/MREPT_code/matlab/code \
+MATLAB_BIN=matlab \
+RADIUS_NII=/home/zcemexx/Scratch/data/invivo/3T_in-vivo_3DGRE_Head_Neck_repeatibility/M1_SNR030.nii.gz \
+ESTIMATE_NOISE=true \
+OUT_NII=/home/zcemexx/Scratch/data/invivo/3T_in-vivo_3DGRE_Head_Neck_repeatibility/cond_pred_M1_SNR030.nii.gz \
+bash /home/zcemexx/projects/MREPT_code/invivo.sh
